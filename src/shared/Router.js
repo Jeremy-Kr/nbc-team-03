daab448f230c9d117a715571c173afd7ed1e5631
Jeremy-kr <wjddlr905@gmail.com>
2022-12-23 09:26:54 +0900

    fix: remove test route
    - 테스트에 진행되던 라우트를 삭제합니다.
    
    by @Jeremy-Kr

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Comment from "../components/comments/Comment";
 import Lending from "../components/Lending/Lending";
 import GlobalStyle from "../components/common/GlobalStyle";
 import Layout from "../components/common/Layout";
@@ -13,7 +12,6 @@ const Router = () => {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route path="/" element={<Lending />} />
-            <Route path="/commentTest" element={<Comment />} />
           </Route>
         </Routes>
       </BrowserRouter>
