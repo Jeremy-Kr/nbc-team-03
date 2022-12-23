c6dbbad453a348d6efce48cbc8dadb1068a9805a
HanulKim <117060618+HanuKim@users.noreply.github.com>
2022-12-23 09:11:33 +0900

    ui: create nav, content list item components
    nav와 content list item 추가함
    
    by @HanuKim

---
+++ src/components/common/ContentListItem.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export default styled.div`
+  margin-top: 12px;
+  padding-left: 20px;
+  display: flex;
+  background-color: #f8f5ef;
+  border-radius: 10px;
+  color: #35353f;
+  border: none;
+  width: 450px;
+  height: 70px;
+  overflow: hidden;
+  font-size: 17px;
+  align-items: center;
+  justify-content: space-around;
+  &:hover {
+    box-shadow: 12px 8px 12px #bb2649;
+    cursor: pointer;
+  }
+`;
---
+++ src/components/common/Nav.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export default styled.nav`
+  margin-top: 12px;
+  display: flex;
+  padding-left: 25px;
+  padding-top: 15px;
+  background-color: #35353f;
+  border: 1px solid #fff;
+  color: #f8f5ef;
+  border: none;
+  width: 505px;
+  height: 70px;
+  overflow: hidden;
+  font-size: 25px;
+  &:hover {
+    cursor: pointer;
+  }
+  align-items: center;
+  justify-content: space-between;
+`;
