0ee5ce77ef607ee36ba8668905c5765140e9dd2b
hiheecho <110316147+hiheecho@users.noreply.github.com>
2022-12-26 09:28:46 +0900

    feat: create post-content feature - 이제 게시물을 등록할수 있습니다.
    by @hiheecho

--- src/redux/modules/contents.js
+++ src/redux/modules/contents.js
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, nanoid } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const name = "contents";
 
@@ -8,10 +9,47 @@ const initialState = {
   error: null,
 };
 
+export const postContent = createAsyncThunk(
+  `${name}/postContent`,
+  async (
+    { nickname, password, contentTitle, contentWhy, contentHow, contentWhen },
+    { fulfillWithValue, rejectWithValue }
+  ) => {
+    try {
+      const res = await axios.post("http://localhost:3001/content", {
+        id: nanoid(),
+        nickname,
+        password,
+        contentTitle,
+        contentWhy,
+        contentHow,
+        contentWhen,
+        comments: [],
+      });
+      return fulfillWithValue(res.data);
+    } catch (e) {
+      return rejectWithValue(e);
+    }
+  }
+);
+
 const contentsSlice = createSlice({
-  name,
+  name: "contents",
   initialState,
   reducers: {},
+  extraReducers: {
+    [postContent.pending]: (state, action) => {
+      state.isLoading = true;
+    },
+    [postContent.fulfilled]: (state, action) => {
+      console.log([...state.contentsData], action.payload);
+      state.isLoading = false;
+    },
+    [postContent.rejected]: (state, action) => {
+      state.error = action.error;
+      state.isLoading = false;
+    },
+  },
 });
 
 const { reducer } = contentsSlice;
